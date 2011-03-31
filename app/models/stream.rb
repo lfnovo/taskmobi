b99c2163f55ee92affe01039a45f85c78206e0a1
Luis Novo <lfnovo@me.com>
2011-03-31 18:53:59 -0300

    done

---
+++ app/models/stream.rb
@@ -0,0 +1,8 @@
+class Stream < ActiveRecord::Base
+    belongs_to :user
+    
+    validates :title, :length => { :maximum =>50 }
+    
+    
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Taskmobi::Application.routes.draw do
+  resources :streams
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

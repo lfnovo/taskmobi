0ff409ecdcf0f752c83119f199aa7449762292c7
Luis Novo <lfnovo@me.com>
2011-03-31 18:31:56 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskmobi::Application.config.secret_token = '9331068343124c26cc632cdee9c62bf29f181742b7339479eb1e277adfbbd996833b40a670ca6058dde9862cc3bcdc54cec3334a26c6f369c2918d411c42c5bd'
